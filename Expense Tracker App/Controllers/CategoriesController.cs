bb0e9026146911057bb2c44af6c4dbbd3f88fa83
Vishalgore07 <vishalgore1604@gmail.com>
2024-09-03 10:40:00 +0530

    initial commit

---
+++ Expense Tracker App/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Expense_Tracker_App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker_App.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return _db.Categories != null ?
+                                    View(await _db.Categories.ToListAsync()) :
+                                    Problem("Entity set 'ApplicationDbContext.Categories' is null.");
+        }
+
+    }
+}
---
+++ Expense Tracker App/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Expense_Tracker_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker_App.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
