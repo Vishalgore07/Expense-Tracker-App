5d3fd1d6a056a7b014e4a59d626d9d8af5131814
Vishalgore07 <vishalgore1604@gmail.com>
2024-09-09 10:29:43 +0530

    Added transaction controller with views

--- Expense Tracker App/Controllers/TransactionsController.cs
+++ Expense Tracker App/Controllers/TransactionsController.cs
@@ -27,10 +27,18 @@ namespace Expense_Tracker_App.Controllers
         }
 
         // GET: Transactions/AddOrEdit
-        public IActionResult AddOrEdit()
+        public IActionResult AddOrEdit(int id=0)
         {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
-            return View();
+            PopulateCategories();
+            if (id == 0)
+            {
+                return View(new Transaction());
+            }
+            else
+            {
+                return View(_context.Transactions.Find(id));
+            }
+            
         }
 
         // POST: Transactions/AddOrEdit
@@ -40,11 +48,19 @@ namespace Expense_Tracker_App.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(transaction);
+                if (transaction.TransactionId == 0)
+                {
+                    _context.Add(transaction);
+                }
+                else
+                {
+                    _context.Update(transaction);
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            //ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            PopulateCategories();
             return View(transaction);
         }
 
@@ -61,6 +77,19 @@ namespace Expense_Tracker_App.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category()
+            {
+                CategoryId = 0,
+                Title = "Choose a Category"
+            };
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = CategoryCollection;
         }
 
     }
--- Expense Tracker App/obj/Debug/net8.0/Expense Tracker App.AssemblyInfo.cs
+++ Expense Tracker App/obj/Debug/net8.0/Expense Tracker App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db895e97d3ad30a64840bca0a942c655604d603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef729c958746a45767b24bf0af65368b6cc6cfdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
